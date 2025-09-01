9317045abf8a99c6ec9072892ac72b5d762e863a
meghana <meghananijanapuram@gmail.com>
2025-09-01 12:50:38 +0530

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -23,6 +23,7 @@ app.use(
     origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
@@ -30,6 +31,7 @@ app.options("*", cors({
   origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 const sessionOptions = {
