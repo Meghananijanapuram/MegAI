406ada427d80760f68b34ac84afb13172a93b8cd
meghana <meghananijanapuram@gmail.com>
2025-09-03 19:37:49 +0530

    Remove node_modules from repo

--- Backend/server.js
+++ Backend/server.js
@@ -16,16 +16,16 @@ const PORT = process.env.PORT || 8080;
 app.use(cookieParser(process.env.SECRET_CODE));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://meg-ai-frontend.vercel.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://meg-ai-frontend.vercel.app");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 
 
 const allowedOrigins = [
