6611360aae87e704e02dd9514c41bb713a67cd6d
meghana <meghananijanapuram@gmail.com>
2025-09-12 19:29:12 +0530

    updated cors

--- Backend/server.js
+++ Backend/server.js
@@ -36,10 +36,10 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins[0].includes(origin)) {
+      if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(new Error("Not allowed by CORS" + origin));
       }
     },
     credentials: true,
